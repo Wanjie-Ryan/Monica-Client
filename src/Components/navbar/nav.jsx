e2c9683f0c04907821b324bb5ea3eb5009fee314
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-17 20:07:05 +0300

    monica

---
+++ src/Components/navbar/nav.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './nav.css'
+
+
+
+
+function Navbar() {
+
+
+  return (
+
+
+    <>
+
+        <nav className ='nav-main'>
+
+            <div className="inner-nav">
+
+
+
+                
+            </div>
+
+
+
+           
+
+
+
+
+
+
+        </nav>
+
+
+    
+    
+    </>
+
+
+  )
+}
+
+export default Navbar
