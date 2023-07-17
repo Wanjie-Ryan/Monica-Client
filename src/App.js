0fdaaaf9591ae2a4a2c3c54856491f6db5d3cd7a
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-17 15:59:05 +0300

    aspirant-integration

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../src/Components/navbar/nav'
+
+
+function App() {
+
+
+  return (
+
+    <>
+
+      <Navbar />
+
+
+    
+    
+    </>
+
+  )
+}
+
+export default App
