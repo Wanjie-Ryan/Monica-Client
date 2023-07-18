b3829e19e97a34b7fbaec125ef6c757cfa6cb0c6
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-18 10:06:16 +0300

    monica

--- src/Components/navbar/nav.jsx
+++ src/Components/navbar/nav.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './nav.css'
-
-
+import {Link} from 'react-router-dom'
+import SiteLogo from '../../Assets/favicon.jpg'
+import {BiHome, BiSolidContact} from 'react-icons/bi'
+import {FcGallery} from 'react-icons/fc'
+import {MdOutlineRememberMe} from 'react-icons/md'
+import {CgProfile} from 'react-icons/cg'
 
 
 function Navbar() {
@@ -16,21 +20,62 @@ function Navbar() {
 
             <div className="inner-nav">
 
+                <div className="site-logo">
+
+                    <div className="flip-box img-logo">
+
+                        <div className="flip-box-inner">
+
+                            <div className="flip-box-front">
+
+                                <img className="logo" src={SiteLogo} alt="logo" />
+
+                            </div> 
+
+                            <div className="flip-box-back">
+   
+                                <p>Be Equipped For Ministry </p>
+                                
+                            </div> 
+
+                        </div>
+
+                    </div>
 
 
-                
+
+                </div>
+
+
+
+                <div className="nav-details">
+
+                    <div className="nav-links">
+
+                        <p className='nav-p'>Home</p>
+                        <p className='nav-p'>Contact</p>
+                        <p className ='nav-p'>Events</p>
+                        <p className='nav-p'>Gallery</p>
+                        <p className='nav-p'>Become a Member</p>
+                        <CgProfile title='Profile' className='icon'/>
+
+
+                    </div>
+
+
+
+                </div>
+
             </div>
 
-
-
-           
-
-
-
-
-
-
         </nav>
+
+        <aside>
+
+            
+
+
+        </aside>
 
 
     
