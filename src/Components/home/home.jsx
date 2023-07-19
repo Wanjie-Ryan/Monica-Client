05ea96cf748c6e827901ca69566c258b4498865e
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-19 20:24:45 +0300

    monica

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -1,15 +1,132 @@
 import React from 'react'
-
+import './home.css'
 
 
 function Home() {
 
 
+  const imageData = [
+
+    {
+
+      id:1,
+      image:'../../Assets//homeImages/vicar.jpg',
+      message: 'Our Leader'
+    }, 
+
+    {
+      id:2,
+      image: '../../Assets/homeImages/Team.jpg',
+      message:'Our Able Team'
+    },
+
+    {
+
+      id:3,
+      image:'../../Assets/homeImages/view.jpg',
+      message:`ACK St.Monica's church Mugumo-ini`
+
+    },
+
+    {
+      id:4,
+      image:'../../Assets/homeImages/view2.jpg',
+      message:`ACK St.Monica's church Mugumo-ini at night`
+    },
+
+    {
+
+      id:5,
+      image:'../../Assets/homeImages/Congregation1.jpg',
+      message:'Our congregation and worshippers'
+
+    },
+
+    {
+
+      id:6,
+      image:'../../Assets/homeImages/worship.jpg',
+      message:'Worship Team'
+
+    },
+
+    {
+
+      id:7,
+      image:'../../Assets/homeImages/drumist.jpg',
+      message:'Fully Equipped musical instruments'
+
+    },
+
+    {
+
+      id:8,
+      image:'../../Assets/homeImages/men2.jpg',
+      message:'The men of St.Monica'
+
+    },
+
+    {
+
+      id:9,
+      image:'../../Assets/homeImages/women.jpg',
+      message:'The women of St.Monica'
+
+    },
+
+    {
+
+      id:10,
+      image:'../../Assets/homeImages/sunday school.jpg',
+      message:`St.Monica's Sunday School`
+
+    },
+
+    {
+
+      id:11,
+      image:'../../Assets/homeImages/kids fellowship.jpg',
+      message:`St.Monica's Kids`
+
+    },
+
+    {
+
+      id:12,
+      image:'../../Assets/homeImages/awards.jpg',
+      message:'What do you know, we also win awards'
+
+    },
+
+
+
+
+  ]
+
+
   return (
 
 
-    <div>Home</div>
+    <>
 
+
+        <section className ='home'>
+
+          <div className="home-intro">
+
+
+
+          </div>
+
+
+
+
+
+        </section>
+    
+    
+    
+    </>
 
 
   )
