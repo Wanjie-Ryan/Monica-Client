747f7ac3a32aadda65c473e74efc786ca4aa761b
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-25 20:01:35 +0300

    monica

--- src/Components/Ministries/Kama/kama.jsx
+++ src/Components/Ministries/Kama/kama.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './kama.css'
-import Men from '../../../Assets/homeImages/men.jpg'
+// import Men from '../../../Assets/homeImages/men.jpg'
+import KamaLogo from '../../../Assets/Ministries/KAMA.png'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
 
 function Kama() {
+
+
+    const KamaData =[
+
+        {
+            id:1,
+            image:require('../../../Assets/homeImages/men.jpg'),
+            
+        },
+
+        {
+            id:2,
+            image:require('../../../Assets/homeImages/men2.jpg')
+        }
+    ]
 
 
 
@@ -15,13 +33,35 @@ function Kama() {
 
         <section className="kama">
 
+           <div className="kama-cont"> <img src={KamaLogo} alt ='kama-logo' className='kamalogo'/></div>
             <p className='kama-title'>KAMA</p>
 
             <div className='kama-container'>
 
                 <div className='kama-img-cont'>
 
-                    <img src ='' alt ='' className=''/>
+                    <Splide
+                        options={{
+                            type: 'fade', // or 'slide' for horizontal sliding effect
+                            perPage: 1,
+                            autoplay: true,
+                            pauseOnHover: false,
+                            loop: true,
+                            // Add more options as needed
+                        }}
+                        >
+                        { KamaData.map((item) => (
+
+                            <SplideSlide key={item.id}>
+
+                            <img src={item.image} alt={item.message} className='img-kama'/>
+                        
+                            </SplideSlide>
+
+                        ))}
+
+                    </Splide>
+
 
 
                 </div>
@@ -29,9 +69,11 @@ function Kama() {
                 <div className="kama-details">
 
 
-                    
-                </div>
 
+
+
+
+                </div>
 
 
 
