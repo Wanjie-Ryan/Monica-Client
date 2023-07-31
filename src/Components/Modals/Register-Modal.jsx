aeb58d275d3f7dd51e6f9e158f88bc8e3688dc1b
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-31 08:56:10 +0300

    monica

--- src/Components/Modals/Register-Modal.jsx
+++ src/Components/Modals/Register-Modal.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './reg.css'
 import Modal from 'react-modal'
-import {BsFillPersonFill, BsFillTelephoneFill} from 'react-icons/bs'
-import {MdEmail} from 'react-icons/md'
-import {BiCurrentLocation} from 'react-icons/bi'
 
 
-function Regsiter({ isOpen, onClose, onUpdate, votername, contact,  onChange, errmsg }) {
+
+function Regsiter({ isOpen, onClose }) {
 
 
   return (
@@ -25,49 +23,11 @@ function Regsiter({ isOpen, onClose, onUpdate, votername, contact,  onChange, er
 
                 <h2>Register New Member</h2>
                 <p className='modal-note'>Note:Only for new members who have <mark>never</mark> been to St.Monica</p>
-
-                <div className ='name'>
-
-                    <BsFillPersonFill/>
-
-                    <input type="text" name="namesvoter" placeholder ='Enter Your name' value={votername} onChange={onChange} />
-
-                </div>
-
-                <div className ='name'>
-
-                    <MdEmail/>
-
-                    <input type="text" name="namesvoter" placeholder ='Enter your email' value={votername} onChange={onChange} />
-
-                </div>
-
-                <div className ='name'>
-
-                    <BiCurrentLocation/>
-
-                    <input type="text" name="namesvoter" placeholder ='Where do you reside?' value={votername} onChange={onChange} />
-
-                </div>
-
-
-                <div className="name">
-                    
-                  <BsFillTelephoneFill/>
-
-
-                  <input type="text" name="contact" placeholder ='Enter Your PhoneNumber' value={contact} onChange={onChange} />
-
-                   
-                </div>
-
-
+                <p className='modal-note'>Download the Registration form below and submit it physically to the church office.</p>
                 
 
-                <button onClick={onUpdate} className='update-modal-button' >Submit</button>
+                <button  className='update-modal-button' > Download form</button>
                 <button onClick={onClose} className ='modal-close-button'>Cancel</button>
-
-                {/* {errmsg && <p className ='error'>{errmsg}</p>} */}
 
         </div>
 
