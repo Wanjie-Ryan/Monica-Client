a855ed13f1692962e8a9e37e5987b1778e4c2f0d
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-10 11:21:23 +0300

    clergy-integration

--- src/Components/Ministries/Mu/mu.jsx
+++ src/Components/Ministries/Mu/mu.jsx
@@ -1,122 +1,98 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 // import './kama.css'
 // import Men from '../../../Assets/homeImages/men.jpg'
-import MuLogo from '../../../Assets/Ministries/Mothers-union.png'
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/splide/dist/css/themes/splide-default.min.css';
-import MuModal from './min-Modal'
-
+import MuLogo from "../../../Assets/Ministries/Mothers-union.png";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+import MuModal from "./min-Modal";
 
 function MothersUnion() {
+  const [isKamaModalOpen, setKamaModalOpen] = useState(false);
 
-    const [isKamaModalOpen, setKamaModalOpen] = useState(false);
+  const openKamaModal = () => {
+    setKamaModalOpen(true);
+  };
 
-    const openKamaModal = () => {
-      setKamaModalOpen(true);
-    };
-  
-    const closeKamaModal = () => {
-      setKamaModalOpen(false);
-    }; 
+  const closeKamaModal = () => {
+    setKamaModalOpen(false);
+  };
 
+  const KamaData = [
+    {
+      id: 1,
+      image: require("../../../Assets/homeImages/women.jpg"),
+    },
 
-    const KamaData =[
-
-        {
-            id:1,
-            image:require('../../../Assets/homeImages/women.jpg'),
-            
-        },
-
-        {
-            id:2,
-            image:require('../../../Assets/homeImages/worship.jpg')
-        }
-    ]
-
-
+    {
+      id: 2,
+      image: require("../../../Assets/homeImages/worship.jpg"),
+    },
+  ];
 
   return (
-
-
     <>
+      <section className="kama">
+        <div className="kama-cont">
+          {" "}
+          <img src={MuLogo} alt="kama-logo" className="kamalogo" />
+        </div>
+        <p className="kama-title">Mothers' Union</p>
 
+        <div className="kama-container">
+          <div className="kama-img-cont">
+            <Splide
+              options={{
+                type: "fade", // or 'slide' for horizontal sliding effect
+                perPage: 1,
+                autoplay: true,
+                pauseOnHover: false,
+                loop: true,
+                // Add more options as needed
+              }}
+            >
+              {KamaData.map((item) => (
+                <SplideSlide key={item.id}>
+                  <img
+                    src={item.image}
+                    alt={item.message}
+                    className="img-kama"
+                  />
+                </SplideSlide>
+              ))}
+            </Splide>
+          </div>
 
-        <section className="kama">
+          <div className="kama-details">
+            <p className="kama-desc-title">About Mothers' Union</p>
 
-           <div className="kama-cont"> <img src={MuLogo} alt ='kama-logo' className='kamalogo'/></div>
-            <p className='kama-title'>Mothers' Union</p>
+            <p className="about-kama">
+              {" "}
+              &bull;Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Incidunt ipsum distinctio fugiat doloremque sit beatae magni
+              libero quod aspernatur eveniet, sunt cupiditate cum dolorem minima
+              dolore qui reiciendis quas atque?
+            </p>
 
-            <div className='kama-container'>
+            <p className="about-kama">
+              {" "}
+              &bull;Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Incidunt ipsum distinctio fugiat doloremque sit beatae magni
+              libero quod aspernatur eveniet, sunt cupiditate cum dolorem minima
+              dolore qui reiciendis quas atque?
+            </p>
 
-                <div className='kama-img-cont'>
+            {/* <div className="btn-container"><button className='kama-reg' onClick={openKamaModal}>Become a MU member?</button></div> */}
+          </div>
+        </div>
+      </section>
 
-                    <Splide
-                        options={{
-                            type: 'fade', // or 'slide' for horizontal sliding effect
-                            perPage: 1,
-                            autoplay: true,
-                            pauseOnHover: false,
-                            loop: true,
-                            // Add more options as needed
-                        }}
-                        >
-                        { KamaData.map((item) => (
-
-                            <SplideSlide key={item.id}>
-
-                            <img src={item.image} alt={item.message} className='img-kama'/>
-                        
-                            </SplideSlide>
-
-                        ))}
-
-                    </Splide>
-
-
-
-                </div>
-
-                <div className="kama-details">
-
-                    <p className="kama-desc-title">About Mothers' Union</p>
-
-                    <p className='about-kama'> &bull;Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsum distinctio fugiat doloremque sit beatae magni libero quod aspernatur eveniet, sunt cupiditate cum dolorem minima dolore qui reiciendis quas atque?</p>
-
-                    <p className='about-kama'> &bull;Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsum distinctio fugiat doloremque sit beatae magni libero quod aspernatur eveniet, sunt cupiditate cum dolorem minima dolore qui reiciendis quas atque?</p>
-
-
-
-                    <div className="btn-container"><button className='kama-reg' onClick={openKamaModal}>Become a MU member?</button></div>
-
-
-
-                </div>
-
-
-
-            </div>
-
-
-
-
-        </section>
-
-
-        <MuModal
+      {/* <MuModal
         
             isOpen={isKamaModalOpen}
             onClose={closeKamaModal}
-        />
-
-
-    
-    
-    
+        /> */}
     </>
-
-
-  )
+  );
 }
 
-export default MothersUnion
+export default MothersUnion;
