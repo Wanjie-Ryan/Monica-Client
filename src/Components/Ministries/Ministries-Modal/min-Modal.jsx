8e02906b3d97f3f929ecc39bf45b7378ba88854b
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-10 11:18:55 +0300

    clergy-integration

--- src/Components/Ministries/Ministries-Modal/min-Modal.jsx
+++ src/Components/Ministries/Ministries-Modal/min-Modal.jsx
@@ -1,40 +1,30 @@
-import React from 'react'
-import './reg.css'
-import Modal from 'react-modal'
-
+import React from "react";
+import "./reg.css";
+import Modal from "react-modal";
 
 function MinistryRegsiter({ isOpen, onClose }) {
-
-
   return (
-
     <Modal
-    
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Register Modal"
       className="modal-content update-modal-content"
       overlayClassName="modal-overlay"
-
     >
+      <div className="modal-content-main">
+        <h2>Register As a KAMA</h2>
 
-        <div className ='modal-content-main'>
+        <p>
+          Download the form below and submit it physically to the church offices
+        </p>
 
-                <h2>Register As a KAMA</h2>
-
-                <p>Download the form below and submit it physically to the church offices</p>
-
-                
-                <button className='update-modal-button' >Download Form</button>
-                <button onClick={onClose} className ='modal-close-button'>Cancel</button>
-
-
-        </div>
-
-
+        <button className="update-modal-button">Download Form</button>
+        <button onClick={onClose} className="modal-close-button">
+          Cancel
+        </button>
+      </div>
     </Modal>
-
-  )
+  );
 }
 
-export default MinistryRegsiter
+export default MinistryRegsiter;
