aaf486e62e050ed448771d8836bd4a640ac0bd51
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-10 11:06:43 +0300

    clergy-integration

--- src/Components/contactModal/cont-modal.jsx
+++ src/Components/contactModal/cont-modal.jsx
@@ -1,117 +1,73 @@
-import React from 'react'
-import './cont-modal.css'
-import Modal from 'react-modal'
+import React from "react";
+import "./cont-modal.css";
+import Modal from "react-modal";
 
-
-
-function ContModal({ isOpen, onClose, }) {
-
-
+function ContModal({ isOpen, onClose }) {
   return (
-
     <Modal
-    
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Contact Modal"
       className="modal-contents update-modal-content"
       overlayClassName="modal-overlay"
-
     >
+      <div className="modal-content-main-contact">
+        <h2>Office Working Hours</h2>
 
-        <div className ='modal-content-main-contact'>
+        <div className="table-container">
+          <table className="custom-table">
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Time</th>
+              </tr>
+            </thead>
 
-            <h2>Office Working Hours</h2>
+            <tbody>
+              <tr className="row-even">
+                <td>Monday</td>
+                <td>8:00Am - 5:00Pm</td>
+              </tr>
 
-            <div className="table-container">
+              <tr className="row-even">
+                <td>Tuesday</td>
+                <td>8:00Am - 5:00Pm</td>
+              </tr>
 
-                <table className="custom-table">
+              <tr className="row-even">
+                <td>Wednesday</td>
+                <td>8:00Am - 5:00Pm</td>
+              </tr>
 
-                  <thead>
+              <tr className="row-even">
+                <td>Thursday</td>
+                <td>8:00 Am - 5:00Pm</td>
+              </tr>
 
-                    <tr>
+              <tr className="row-even">
+                <td>Friday</td>
+                <td>8:00 Am - 5:00Pm</td>
+              </tr>
 
-                      <th>Day</th>
-                      <th>Time</th>
-                    
-                    </tr>
+              <tr className="row-even">
+                <td>Saturday</td>
+                <td>Closed</td>
+              </tr>
 
-                  </thead>
-
-                  <tbody>
-
-                    <tr className="row-even">
-
-                      <td>Monday</td>
-                      <td>8:00Am - 5:00Pm</td>
-                      
-                    </tr>
-
-                    <tr className="row-even">
-
-                      <td>Tuesday</td>
-                      <td>8:00Am - 5:00Pm</td>
-                      
-
-                    </tr>
-
-                    <tr className="row-even">
-
-                      <td >Wednesday</td>
-                      <td>8:00Am - 5:00Pm</td>
-                      
-
-                    </tr>
-
-                    <tr className="row-even">
-
-                      <td>Thursday</td>
-                      <td>8:00 Am - 5:00Pm</td>
-                      
-                    </tr>
-
-                    <tr className="row-even">
-
-                      <td>Friday</td>
-                      <td>8:00 Am - 5:00Pm</td>
-                      
-                    </tr>
-
-                    <tr className="row-even">
-
-                      <td>Saturday</td>
-                      <td>Closed</td>
-                      
-
-                    </tr>
-
-                    <tr className="row-even">
-
-                      <td>Public Holdiays</td>
-                      <td>Closed</td>
-                      
-
-                    </tr>
-
-                   
-                  </tbody>
-
-                </table>
-
-             </div>
-
-
-  
-            <button onClick={onClose} className ='modal-close-button'>Cancel</button>
-
-                
-
+              <tr className="row-even">
+                <td>Public Holdiays</td>
+                <td>Closed</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-
+        <button onClick={onClose} className="modal-close-button">
+          Cancel
+        </button>
+      </div>
     </Modal>
-
-  )
+  );
 }
 
-export default ContModal
+export default ContModal;
